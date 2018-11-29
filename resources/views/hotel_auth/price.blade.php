af22b730ec949e912ce5dcbc0c26684d094cb414
jj <jj@kurrency.com>
2018-11-29 19:11:16 +0800

    修改

--- resources/views/hotel_auth/price.blade.php
+++ resources/views/hotel_auth/price.blade.php
@@ -13,16 +13,70 @@
 <!-- 隱藏區塊 -->
 @endif
 
+<div style="text-align: center;font-weight: bolder;">
+	<span>下面勾選項目，設定為 客滿</span> 
+</div>
+
 <div class="row" style="width: 98%;margin: auto;">
-	test
+	<table class="header" style="width: 100%;text-align: center;">
+    	<thead><th> <a href=""><<</a>  <a href=""><</a> 03月  <a href="">></a>  <a href="">>></a> </th></thead>
+    </table>
+</div>
+<div class="row" style="width: 98%;margin: auto;">
+	<div class="scrolltable">
+        <div class="body">
+            <table width="100%">
+                <tbody>
+                <tr>
+                	<td>房型</td><td>定價</td><td>人數</td>
+                </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 <!-- main -->
 
 @endsection
 
 @section('instyle')
-
-
+.scrolltable {
+    overflow-x: scroll;
+    height: 100%;
+    display: flex;
+    display: -webkit-flex;
+    flex-direction: column;
+    -webkit-flex-direction: column;
+}
+.scrolltable > .body {
+    /*noinspection CssInvalidPropertyValue*/
+    width: -webkit-fit-content;
+    overflow-y: scroll;
+    flex: 1;
+    -webkit-flex: 1;
+}
+table {
+    border-collapse: collapse;
+}
+th, td {
+  border: 1px solid #bbb;
+  min-width: 150px;
+  padding: 8px 16px;
+}
+th {
+    background-color: lightgrey;
+    border-width: 1px;
+}
+td {
+    border-width: 1px;
+}
+tr:first-child td {
+    border-top-width: 0;
+}
+tr:nth-child(even) {
+    background-color: #eee;
+}
 @endsection
 
 <!-- js獨立區塊腳本 -->
